7673248bf9b64df4f93c72d24b0bf0d8f9aabb27
Tautvydas Dulskis <tautvydas@dulskis.eu>
2023-03-28 19:15:48 +0300

    Navigacijos kodas JS'e

--- js/navigation.js
+++ js/navigation.js
@@ -1,43 +1,96 @@
-let meniu = [
-    {
-        pavadinimas: 'Home',
-        nuoroda: 'index.html'
-    },
-    {
-        pavadinimas: 'About',
-        nuoroda: 'about.html',
-    },
-    {
-        nuoroda: 'services.html',
-        pavadinimas: 'Services'
+function gautiMeniuSarasa(meniuVieta) {
+    let meniu = [];
+    // Kuriame elemente yra UL tagas
+    let kurYraMeniu = meniuVieta.parentElement.parentElement.tagName;
 
-    },
-    {
-        pavadinimas: 'Contact',
-        nuoroda: 'contact.html'
+    // Pagal tevini taga grazinti atitinkama meniu
+    switch (kurYraMeniu) {
+        case 'HEADER':
+            meniu = [
+                {
+                    pavadinimas: 'Home',
+                    nuoroda: 'index.html'
+                },
+                {
+                    pavadinimas: 'About',
+                    nuoroda: 'about.html',
+                },
+                {
+                    nuoroda: 'services.html',
+                    pavadinimas: 'Services'
+
+                },
+                {
+                    pavadinimas: 'Contact',
+                    nuoroda: 'contact.html'
+
+                }
+            ];
+            break;
+        case 'FOOTER':
+            meniu = [
+                {
+                    pavadinimas: 'About',
+                    nuoroda: 'about.html',
+                },
+                {
+                    nuoroda: 'services.html',
+                    pavadinimas: 'Services'
+
+                },
+                {
+                    nuoroda: 'products.html',
+                    pavadinimas: 'Products'
+
+                },
+                {
+                    pavadinimas: 'Contact',
+                    nuoroda: 'contact.html'
+
+                }
+            ];
+            break;
     }
-];
 
-let navigacija = document.querySelector('nav > ul');
-
-let aktyvusPuslapis = document.location.href.split('#')[1];
-
-for (let i = 0; i < meniu.length; i++) {
-    let meniuElementas = document.createElement('li');
-    let nuoroda = document.createElement('a');
-
-    let elem = meniu[i];
-
-    if (elem.pavadinimas === aktyvusPuslapis) {
-        meniuElementas.classList.add('aktyvus');
-    }
-
-    let urlAdresas = elem.nuoroda + '#' + elem.pavadinimas;
-
-    nuoroda.setAttribute('href', urlAdresas);
-    nuoroda.innerHTML = elem.pavadinimas;
-
-    meniuElementas.appendChild(nuoroda);
-
-    navigacija.appendChild(meniuElementas);
+    return meniu;
 }
+
+function gautiPuslapioPavadinima() {
+    return document.location.href.split('#')[1];
+}
+
+function generuotiMeniu(meniuDuomenys, meniuVieta){
+    // Nustatome kuris puslapis šiuo metu yra aktyvus
+    let aktyvusPuslapis = gautiPuslapioPavadinima();
+
+    for (let i = 0; i < meniuDuomenys.length; i++) {
+        let elem = meniuDuomenys[i];
+
+        let naujasMeniuElementas = document.createElement('li');
+        let nuoroda = document.createElement('a');
+
+        if (elem.pavadinimas === aktyvusPuslapis) {
+            naujasMeniuElementas.classList.add('aktyvus');
+        }
+
+        let urlAdresas = elem.nuoroda + '#' + elem.pavadinimas;
+
+        nuoroda.setAttribute('href', urlAdresas);
+        nuoroda.innerHTML = elem.pavadinimas;
+
+        naujasMeniuElementas.appendChild(nuoroda);
+
+        meniuVieta.appendChild(naujasMeniuElementas);
+    }
+}
+
+// Nurodome kuriame elemente norime spausdinti savo meniu
+let meniuVietaTop = document.querySelector('header > nav > ul');
+let meniuVietaBottom = document.querySelector('footer > nav > ul');
+
+// Gauname Menių sarasa, pagal tai kuris elementas yra tevinis, meniuVietaTop elemento atžvilgiu
+let meniuSarasasTop = gautiMeniuSarasa(meniuVietaTop);
+let meniuSarasasBottom = gautiMeniuSarasa(meniuVietaBottom);
+
+generuotiMeniu(meniuSarasasTop, meniuVietaTop);
+generuotiMeniu(meniuSarasasBottom, meniuVietaBottom);
