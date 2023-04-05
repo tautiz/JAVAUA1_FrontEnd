fcc786ff73697145046b46b477ebf68478690579
Tautvydas Dulskis <tautvydas@dulskis.eu>
2023-04-05 20:01:28 +0300

    KOntaktu forma su Useriais

---
+++ js/main.js
@@ -0,0 +1,40 @@
+function createUserElement (data) {
+    console.log('createUserElement()');
+  var user = document.createElement('div');
+  user.className = 'user';
+  let fullName = data.first_name + ' ' + data.last_name + ' ' + data.email
+
+  user.innerHTML = '<img src="' + data.avatar + '" alt="' + fullName + '" class="user-avatar">&nbsp;<span>' + fullName + '</span>';
+
+  return user;
+}
+
+function getUsers(url, callback) {
+  fetch(url)
+    .then(response => response.json())
+    .then(duomenys => {
+        console.log('Gavau duomenis ir kvieciu Callback f-ja');
+        callback(duomenys);
+        console.log('Baigesi Callback f-ja');
+    });
+}
+
+function appendToUsersElement(duomenys) {
+    console.log('appendToUsersElement()');
+    let users = document.getElementById('users');
+    users.innerHTML = '';
+
+    duomenys.data.forEach(user => {
+
+        let naujasVartotojas = createUserElement(user);
+
+        users.appendChild(naujasVartotojas);
+        console.log('Pridetas vartotojas');
+    });
+}
+
+const url = 'https://reqres.in/api/users?delay=3';
+
+console.log('Pradedu uzklausa');
+getUsers(url, appendToUsersElement);
+console.log('Baigesi JS kodas');
